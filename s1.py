4ec8a2c2e2b769474220c77571e0a1eb39390dc0
ShubhamYerudkar <136616325+ShubhamYerudkar@users.noreply.github.com>
2023-06-14 21:06:22 +0530

    Add files via upload

---
+++ s1.py
@@ -0,0 +1,11 @@
+print("first code")
+a=10
+print("number of lines it contains: ",a)
+n=input("enter your name: ")
+print("student name: ",n)
+m1=int(input("enter your marks"))
+m2=int(input())
+m3=int(input())
+print("marks: ",m1,m2,m3)
+avg=(m1+m2+m3)/3
+print("average: ",avg)
